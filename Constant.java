e6e20507f0efe25c1ba393e2ef4bd4e964db9bb7
KirtankumarPatel <133532645+KirtankumarPatel@users.noreply.github.com>
2023-08-18 11:54:41 -0400

    Add files via upload

---
+++ Constant.java
@@ -0,0 +1,21 @@
+package com.kirtanpatel.foodapp;
+
+
+public class Constant {
+    public static final String UserBucket = "User";
+    public static final String OrderBucket = "Orders";
+    public static final String Mobile = "Mobile";
+    public static final String Password = "Password";
+    public static final String IsLogin = "LogStatus";
+    public static final String IsFirstTime = "FirstTime";
+    public static final String Category = "Category";
+    public static final String Kitchen = "Kitchen";
+    public static final String SharedPrefName = "FoodApp";
+    public static final String CartPrefName = "FoodAppCart";
+    public static final String PAYMENT_URL = "https://friendvilla.in/foodapp/pay.php?CUST_ID=";
+    public static final String INTERFACE = "azbasdeftghukoiytsxcvbklpoiwertyuioplkjhgzxcbnr";
+    public static final String Name = "Name";
+    public static final String Phone = "Phone";
+    public static final String Address = "Address";
+    public static final String Landmark = "Landmark";
+}
